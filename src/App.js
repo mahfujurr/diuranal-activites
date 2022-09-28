9abea73c32371050e9fd13e0a3674a641f961cda
muradxkira <mahfujur797@gmail.com>
2022-09-28 11:41:29 -0400

    added fake data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Component from './Components/Components/Component';
 
 function App() {
   return (
     <div>
-      
+      <Component></Component>
     </div>
   );
 }
